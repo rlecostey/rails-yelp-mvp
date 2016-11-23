ae56753f9edeb96749de4c4ffab36343eeed2772
remy lecostey <remy.lecostey@gmail.com>
2016-11-23 17:03:29 +0100

    Complete Yelp Exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    get 'reviews/new', to: "reviews#new"
+    post 'reviews', to: "reviews#create"
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Temple d'Angkor",
+    address:      "Nantes",
+    phone_number: "0656352617",
+    category:     "chinese"
+  },
+  {
+    name:         "Sushi shop",
+    address:      "Nantes",
+    phone_number: "0656352317",
+    category:     "japanese"
+  },
+  {
+    name:         "Chez Luigi",
+    address:      "Nantes",
+    phone_number: "0654052617",
+    category:     "italian"
+  },
+  {
+    name:         "Le Dubrown",
+    address:      "Nantes",
+    phone_number: "0656359417",
+    category:     "french"
+  },
+  {
+    name:         "Le coup fourré",
+    address:      "Nantes",
+    phone_number: "0656371617",
+    category:     "french"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
